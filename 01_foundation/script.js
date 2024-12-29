b62c6940aea886c7723aaf9668ee4512dad34cd5
Camila Alves <alvescamila87@gmail.com>
2024-12-29 20:00:46 -0300

    typeof

---
+++ 01_foundation/script.js
@@ -0,0 +1,32 @@
+//tipos de dados
+
+var nome = 'Camila'
+var idade = 36
+var time; //undefined
+var time = null; //objeto
+var simbolo = Symbol()
+
+console.log(simbolo)
+console.log(typeof simbolo)
+
+// soma strings
+
+var sobrenome = "Abraão"
+var nomeCompleto = nome + " " + sobrenome
+console.log(nomeCompleto)
+
+// number
+
+var ano = "2024"
+var mes = 8
+console.log(ano + mes)
+
+// back slash
+
+var frase = "Esse é o \"melhor\" jogo!"
+console.log(frase)
+
+var frase2 = "It's time! " + ano
+console.log(frase2)
+
+console.log(typeof frase2)
