e015aac8dd93ef6ae7fb658fc80457ba8f75371b
Camila Alves <alvescamila87@gmail.com>
2024-12-30 17:03:29 -0300

    falsy and truthy

---
+++ 01_foundation/04_boolean/script.js
@@ -0,0 +1,38 @@
+//boolean
+
+var nome = ""; //valor falso
+
+if(nome) {
+    console.log(nome)
+} else {
+    console.log("Não existe nome")
+}
+
+//falsy
+
+/* 
+
+    if(false)
+    if(0) // ou -0
+    if(NaN)
+    if(null)
+    if(undefined)
+    if('undefined') // ou "" ou `` 
+
+// todo o resto da truthy
+
+*/
+
+//truthy
+
+/* 
+
+    if(true)
+    if(1) 
+    if(' ')
+    if('camila')
+    if(-5)
+    if({}) 
+
+*/
+
