09fec91502c03e4bf50126a840ae16ad02a83462
Camila Alves <alvescamila87@gmail.com>
2025-01-01 21:16:00 -0300

    inheritance, object, function with params

--- 01_foundation/06_objetos/script.js
+++ 01_foundation/06_objetos/script.js
@@ -69,3 +69,63 @@ menu.esconder = function() {
 }
 
 console.log(menu.esconder())
+
+//this
+var height = 100;
+var menu2 = {
+    width: 900,
+    height: 50,
+    backgroundColor: '#84E',
+    metadeHeight: () => {
+        return this.height / 2 // divisão do height propriedade
+        // return height / 2 // divisão do height de fora do objeto
+    }
+}
+
+// exercício 2:
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+var pai = {
+    nome: 'Paulo',
+    sobrenome: 'Alves',
+    idade: 63,
+    possuirMBA: true,
+    quantidadeMBA: 2,
+}
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+pai.exibirNomeCompleto = function() {
+    //console.log(pai.nome + " " + pai.sobrenome)
+    return `${this.nome} ${this.sobrenome}`
+}
+
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+}
+
+carro.preco = 3000;
+  
+  // Crie um objeto de um cachorro que represente um labrador,
+  // preto com 10 anos, que late ao ver um homem
+
+  var cachorro = {
+    raca: 'labrador',
+    cor: 'preto',    
+    idade: 10,
+    latir(pessoa) {
+        if(pessoa === 'homem') {
+            return 'late: au au'
+        } else {
+            return 'cão amigo'
+        }
+        //console.log('Au au')
+    }
+  }
+  
+  
