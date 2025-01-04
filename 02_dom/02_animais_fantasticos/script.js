b16169cd1b39bd7491227fbcbdf0ba93aa457e6d
Camila Alves <alvescamila87@gmail.com>
2025-01-04 20:30:14 -0300

    html collection and nodelist

--- 02_dom/02_animais_fantasticos/script.js
+++ 02_dom/02_animais_fantasticos/script.js
@@ -21,3 +21,30 @@ console.log("Links: ", linkInterno)
 
 const animaisImg = document.querySelectorAll('.animais img')
 console.log("img: ", animaisImg)
+
+// exercicios
+
+// Retorne no console todas as imagens do site
+const imagensWebsite = document.querySelectorAll('img')
+console.log("Imagens do site: ", imagensWebsite)
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imgWebsite = document.querySelectorAll('img[src^="img/imagem"')
+console.log("Retornar só as imagens: ", imgWebsite)
+
+// Selecione todos os links internos (onde o href começa com #)
+const hrefsHashtag = document.querySelectorAll('a[href^="#"')
+console.log("Todos # : ", hrefsHashtag)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const h2Epecifico = document.querySelector('.animais-descricao h2')
+console.log("H2: ", h2Epecifico)
+
+// Selecione o último p do site
+const ultimoParagrafo = document.querySelector('footer p')
+console.log("último p: ", ultimoParagrafo)
+
+// ou
+const paragrafos = document.querySelectorAll('p')
+console.log('último p2: ', paragrafos[paragrafos.length -1])
+
