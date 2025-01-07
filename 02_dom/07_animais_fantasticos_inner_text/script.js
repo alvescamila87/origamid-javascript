3d4a2cc95921af04d9513642cf88584c59744379
Camila Alves <alvescamila87@gmail.com>
2025-01-07 19:51:36 -0300

    transversing and innetText

---
+++ 02_dom/07_animais_fantasticos_inner_text/script.js
@@ -0,0 +1,46 @@
+// Traversing e Manipulação
+
+const h1 = document.querySelector('h1')
+const animaisLista = document.querySelector('.animais-descricao')
+
+console.log("HTML" , h1.innerHTML)
+console.log("text" , h1.innerText)
+
+console.log("animais html" , animaisLista.innerHTML)
+console.log("animais text" , animaisLista.innerText)
+
+console.log("HTML" , h1.innerHTML)
+console.log("text" , h1.innerText)
+console.log("outer" , h1.outerHTML)
+
+
+h1.outerHTML = `<p>Novo título</p>`
+
+
+// outerHTML, innerHTML e innerText
+const menu = document.querySelector('.menu');
+
+menu.outerHTML; // todo o html do elemento
+menu.innerHTML; // html interno
+menu.innerText; // texto, sem tags
+
+menu.innerText = '<p>Texto</p>'; // a tag vai como texto
+menu.innerHTML = '<p>Texto</p>'; // a tag é renderizada
+
+// Trasversing
+
+const lista = document.querySelector('.animais-lista');
+
+lista.parentElement; // pai
+lista.parentElement.parentElement; // pai do pai
+lista.previousElementSibling; // elemento acima
+lista.nextElementSibling; // elemento abaixo
+
+lista.children; // HTMLCollection com os filhos
+lista.children[0]; // primeiro filho
+lista.children[--lista.children.length]; // último filho
+
+lista.querySelectorAll('li'); // todas as LI's
+lista.querySelector('li:last-child'); // último filho
+
+
