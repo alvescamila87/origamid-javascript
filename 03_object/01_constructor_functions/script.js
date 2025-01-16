1cb180715dc2ee8ae7a7a17c5462538db83b0a62
Camila <alvescamila87@gmail.com>
2025-01-15 21:58:00 -0300

    this keyword in object

--- 03_object/01_constructor_functions/script.js
+++ 03_object/01_constructor_functions/script.js
@@ -31,22 +31,24 @@ const fiat = new Carro(); // criar objeto 2
 
 fiat.marca = "Fiat" 
 
-// const gurgel = new Carro();
+// new keyword
 
-// // 1 Cria um novo objeto
-// gurgel = {};
+const gurgel = new Carro();
 
-// // 2 Define o protótipo
-// gurgel.prototype = Carro.prototype;
+// 1 Cria um novo objeto
+//gurgel = {};
 
-// // 3 Aponta a variável this para o objeto
-// this = gurgel;
+// 2 Define o protótipo
+gurgel.prototype = Carro.prototype;
 
-// // 4 Executa a função, substituindo this pelo objeto
-// gurgel.marca = 'Marca';
-// gurgel.preco = 0;
+// 3 Aponta a variável this para o objeto
+//this = gurgel;
 
-// // 5 Retorna o novo objeto
+// 4 Executa a função, substituindo this pelo objeto
+gurgel.marca = 'Marca';
+gurgel.preco = 0;
+
+// 5 Retorna o novo objeto
 // return gurgel = {
 //   marca: 'Marca',
 //   preco: 0,
@@ -58,4 +60,17 @@ function Animal(racaAtribuida, nomeAtribuido) {
 }
 
 const cachorro = new Animal("pinscher", "marlene");
-console.log(cachorro)
+console.log(cachorro);
+
+
+// this keyword
+function Carro2(marca, precoInicial) {
+    const taxa = 1.2;
+    const precoFinal = precoInicial * taxa;
+    //this.taxa = 1.5;
+    //const precoFinal = precoInicial * this.taxa
+    this.marca = marca; // só acessa issp
+    this.preco = precoFinal; // só acessa issp
+}
+
+const monza = new Carro2('Monza', 75000)
