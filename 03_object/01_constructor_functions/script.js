df8d29e48ffbb5ff84a1d28e114c0bd29856959b
Camila <alvescamila87@gmail.com>
2025-01-15 21:36:52 -0300

    new keyword

--- 03_object/01_constructor_functions/script.js
+++ 03_object/01_constructor_functions/script.js
@@ -16,8 +16,46 @@ vw.preco = 35000
 // criar objeto
 
 function Carro() {
-
+    this.marca = "BWM";
+    this.modelo = "320 M3";
+    console.log(this)
 }
 
-const bmw = new Carro(); // cria objeto
-bmw.teste = "pneus 17'" // cria propriedade
+const bmw = new Carro(); // cria objeto 1
+const fiat = new Carro(); // criar objeto 2
+
+// bmw.teste = "pneus 17'" // cria propriedade
+// bmw.andar = function() {
+//      console.log("andou!")
+// }
+
+fiat.marca = "Fiat" 
+
+// const gurgel = new Carro();
+
+// // 1 Cria um novo objeto
+// gurgel = {};
+
+// // 2 Define o protótipo
+// gurgel.prototype = Carro.prototype;
+
+// // 3 Aponta a variável this para o objeto
+// this = gurgel;
+
+// // 4 Executa a função, substituindo this pelo objeto
+// gurgel.marca = 'Marca';
+// gurgel.preco = 0;
+
+// // 5 Retorna o novo objeto
+// return gurgel = {
+//   marca: 'Marca',
+//   preco: 0,
+// }
+
+function Animal(racaAtribuida, nomeAtribuido) {
+    this.raca = racaAtribuida;
+    this.nome = nomeAtribuido;
+}
+
+const cachorro = new Animal("pinscher", "marlene");
+console.log(cachorro)
