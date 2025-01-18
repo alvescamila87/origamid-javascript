00116fe3eaac65b34398b4f3bae85c9d04218a25
Camila Alves <alvescamila87@gmail.com>
2025-01-17 21:30:39 -0300

    prototype, return, constructor

--- 03_object/03_prototype/script.js
+++ 03_object/03_prototype/script.js
@@ -30,3 +30,43 @@ console.log(camila.prototype) // é um objeto e não uma função
 // acesso ao this.nome
 camila.andar();
 camila.__proto__.andar();
+
+
+// Construtor nativo
+
+const pais = "Brasil"
+console.log(pais.length)
+
+const cidade = new String(`Blumenau`);
+console.log(cidade)
+
+// métodos de array
+const listaAnimais = ['cachorro', 'gato', 'cavalo']
+
+const lista = document.querySelectorAll('li') //nodelist 
+
+const listaArray = Array.prototype.slice.call(lista) // transformar em array
+const listaArray2 = Array.from(lista) // transformar em array - forma 2
+
+// ENTENDER O QUE RETORNA
+
+const carro = {
+    marca: "Volkswagen",
+    pneus: 4,
+    andar: function() {
+        return true;
+    },
+    estacionar: function() {
+        return 'Stopped!'
+    }
+}
+
+console.log(carro.marca.toUpperCase()) // retorno string, acessa métodos percententes ao string
+console.log(carro.pneus.toFixed()) // retorno number, acessa métodos percententes ao number
+console.log(carro.andar()) // retorno booleano, nesse caso, então, acessa métodos percententes ao boolean
+
+
+console.log(carro.andar.constructor) // Function()
+console.log(carro.andar.constructor.name) // function
+console.log(carro.andar().constructor.name) // boolean
+
