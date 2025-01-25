8f592a3147cc5c3b59c11c8e2771b7186142d0c3
Camila Alves <alvescamila87@gmail.com>
2025-01-24 21:35:11 -0300

    number and math

---
+++ 03_object/06_number_math/script.js
@@ -0,0 +1,6 @@
+// number and math
+
+console.log(Number.isNaN(NaN)) // false
+console.log(Number.isNaN(5)) // false
+console.log(Number.isInteger(13)) // true
+console.log(Number.isInteger(13.9)) // false
