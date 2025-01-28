a89f8be9811118456ab017fcab2b44813d815fb6
Camila <alvescamila87@gmail.com>
2025-01-27 21:48:51 -0300

    array, return, destructor

--- 03_object/07_array/script.js
+++ 03_object/07_array/script.js
@@ -41,3 +41,40 @@ new Array(5); // [,,,,]
 Array(5); // [,,,,]
 Array(1,2,3,4); // [1,2,3,4]
 
+//length
+
+const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']];
+frutas.length //3 itens dentro do array de frutas
+frutas[0].length // 6 letras de banana
+frutas[1].length // 4 letras de pera
+frutas[2].length // 2 itens dentro do array
+frutas[2][0].length // 6 letras em uva rota de dentro do array
+
+// mutator methods: retorna valor e altera array original
+// não funciona para array de numbers
+
+const instrumentos2 = ['Guitarra', 'Baixo', 'Violão', 'Amplificador', 'Microfone']
+console.log(instrumentos2.sort());
+
+const idades = [32,21,33,43,1,12,8];
+console.log(idades.sort()) //não funciona -> [1, 12, 21, 32, 33, 43, 8]
+
+//[].unshift() e [].push() -> retorna o length
+const carros2 = ['Ford', 'Fiat', 'VW'];
+carros2.unshift('Honda', 'Kia'); // 5
+carros2; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW'];
+
+carros2.push('Ferrari'); // 6
+carros2; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
+
+//[].shift() e [].pop() -> retorna o array
+const carros3 = ['Ford', 'Fiat', 'VW', 'Honda'];
+const primeiroCarro = carros3.shift(); // 'Ford'
+carros3; // ['Fiat', 'VW', 'Honda'];
+
+const ultimoCarro = carros3.pop(); // 'Honda'
+carros3; // ['Fiat', 'VW'];
+
+//[].reverse() -> retorna nova array
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
