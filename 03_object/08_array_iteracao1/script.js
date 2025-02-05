0592eb812deb5e7d61c5003cfd8888dcedacc965
Camila <alvescamila87@gmail.com>
2025-02-04 22:34:51 -0300

    filter

--- 03_object/08_array_iteracao1/script.js
+++ 03_object/08_array_iteracao1/script.js
@@ -311,3 +311,73 @@ const frutas5 = ['Uva', '', 'Mamão', 'Banana', 'Laranja', undefined, null, fals
 const listaDeFrutasExistentes = frutas5.filter(item => item)
 
 console.log(listaDeFrutasExistentes)
+
+const numerosLista = [45, 20, 30, 55, 100, 2, 3, 13, 46]
+
+const maioresQue45 = numerosLista.filter(item => item > 45)
+
+console.log(maioresQue45) // (3) [55, 100, 46]
+
+const aulas3 = [
+  {
+    nome: 'HTML',
+    min: 15
+  },
+  {
+    nome: 'Java',
+    min: 10
+  },
+  {
+    nome: 'Python',
+    min: 20
+  },
+  {
+    nome: 'JS',
+    min: 25
+  },
+]
+
+const maioresQue15 = aulas3.filter(item => item.min > 15)
+
+console.log(maioresQue15) //(3) [{}, {}] 
+
+// exercícios
+
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+
+
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
