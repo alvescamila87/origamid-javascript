de6f0d94cf64e93b4420642c0dbb0db0625fb0d5
Camila Alves <alvescamila87@gmail.com>
2025-02-13 22:21:38 -0300

    then, catch, finally

--- 05_javascript_assincrono/01_promises/script.js
+++ 05_javascript_assincrono/01_promises/script.js
@@ -1,7 +1,70 @@
 //Promises
 const promessa = new Promise(function(resolve, reject) {
-  //resolve("Camila")
-  reject(Error("Um erro ocorreu na promisse"))
+  let condicao = true;
+  if(condicao) {
+    resolve("Resolve a promessa e ativa a promessa")
+  } else {
+    reject(Error("Um erro ocorreu na promisse"))
+  }
 })
 
 console.log(promessa)
+
+//then: fica esperando a promise ser resolvida, ou seja, ser ativada pelo resolve.
+promessa.then(function(resolucao) {
+  console.log(resolucao)
+})
+
+// assincrono só faz sentido com settimeout
+
+const promessa2 = new Promise(function(resolve, reject) {
+  //let condicao = true;
+  let condicao = false; 
+  if(condicao) {
+    setTimeout(() => {
+      resolve({ nome: "Camila", idade: 36 })
+    }, 1000)
+  } else {
+    reject(Error("Um erro ocorreu na promisse"))
+  }
+})
+
+console.log(promessa)
+
+//then: fica esperando a promise ser resolvida, ou seja, ser ativada pelo resolve.
+const retorno = promessa2.then(function(resolucao) {
+  console.log(resolucao)
+  return 'Teste'
+}).then(resolucao => {
+  console.log(resolucao)
+})
+
+console.log(retorno)
+
+//catch: fica esperando a promise ser rejeitada
+const retorno2 = promessa2.then(resolucao => {
+  resolucao.profissao = 'Dev Software'
+  return resolucao
+}).then(resolucao => {
+  console.log(resolucao)
+}).catch(rejeitada => [
+  console.log(rejeitada)
+])
+
+console.log(retorno)
+
+// catch dentro de then:
+promessa.then(resolucao => {
+  console.log(resolucao);
+}, reject => {
+  console.log(reject);
+});
+
+//finally: sempre executado
+promessa.then(resolucao => {
+  console.log(resolucao);
+}, reject => {
+  console.log(reject);
+}).finally(
+  console.log("Sempre executado!")
+)
