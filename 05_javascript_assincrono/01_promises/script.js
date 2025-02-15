0021b42f5980c66bb8c85bb780b4017611532a3f
Camila Alves <alvescamila87@gmail.com>
2025-02-14 21:05:32 -0300

    promise.all

--- 05_javascript_assincrono/01_promises/script.js
+++ 05_javascript_assincrono/01_promises/script.js
@@ -1,70 +1,92 @@
-//Promises
-const promessa = new Promise(function(resolve, reject) {
-  let condicao = true;
-  if(condicao) {
-    resolve("Resolve a promessa e ativa a promessa")
-  } else {
-    reject(Error("Um erro ocorreu na promisse"))
-  }
+// //Promises
+// const promessa = new Promise(function(resolve, reject) {
+//   let condicao = true;
+//   if(condicao) {
+//     resolve("Resolve a promessa e ativa a promessa")
+//   } else {
+//     reject(Error("Um erro ocorreu na promisse"))
+//   }
+// })
+
+// console.log(promessa)
+
+// //then: fica esperando a promise ser resolvida, ou seja, ser ativada pelo resolve.
+// promessa.then(function(resolucao) {
+//   console.log(resolucao)
+// })
+
+// // assincrono só faz sentido com settimeout
+
+// const promessa2 = new Promise(function(resolve, reject) {
+//   let condicao = true;
+//   //let condicao = false; 
+//   if(condicao) {
+//     setTimeout(() => {
+//       resolve({ nome: "Camila", idade: 36 })
+//     }, 1000)
+//   } else {
+//     reject(Error("Um erro ocorreu na promisse"))
+//   }
+// })
+
+// console.log(promessa)
+
+// //then: fica esperando a promise ser resolvida, ou seja, ser ativada pelo resolve.
+// const retorno = promessa2.then(function(resolucao) {
+//   console.log(resolucao)
+//   return 'Teste'
+// }).then(resolucao => {
+//   console.log(resolucao)
+// })
+
+// console.log(retorno)
+
+// //catch: fica esperando a promise ser rejeitada
+// const retorno2 = promessa2.then(resolucao => {
+//   resolucao.profissao = 'Dev Software'
+//   return resolucao
+// }).then(resolucao => {
+//   console.log(resolucao)
+// }).catch(rejeitada => [
+//   console.log(rejeitada)
+// ])
+
+// console.log(retorno)
+
+// // catch dentro de then:
+// promessa.then(resolucao => {
+//   console.log(resolucao);
+// }, reject => {
+//   console.log(reject);
+// });
+
+// //finally: sempre executado
+// promessa.then(resolucao => {
+//   console.log(resolucao);
+// }, reject => {
+//   console.log(reject);
+// }).finally(
+//   console.log("Sempre executado!")
+// )
+
+// promise.all()
+const login = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('Usuário logado')
+  }, 1000)
 })
 
-console.log(promessa)
-
-//then: fica esperando a promise ser resolvida, ou seja, ser ativada pelo resolve.
-promessa.then(function(resolucao) {
-  console.log(resolucao)
+const dados = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('Dados carregados')
+  }, 2000)
 })
 
-// assincrono só faz sentido com settimeout
+// passa o objeto da promise com um array com o nome das promessas
+const carregouTudo = Promise.all([login, dados])
 
-const promessa2 = new Promise(function(resolve, reject) {
-  //let condicao = true;
-  let condicao = false; 
-  if(condicao) {
-    setTimeout(() => {
-      resolve({ nome: "Camila", idade: 36 })
-    }, 1000)
-  } else {
-    reject(Error("Um erro ocorreu na promisse"))
-  }
-})
+console.log(carregouTudo)
 
-console.log(promessa)
-
-//then: fica esperando a promise ser resolvida, ou seja, ser ativada pelo resolve.
-const retorno = promessa2.then(function(resolucao) {
-  console.log(resolucao)
-  return 'Teste'
-}).then(resolucao => {
+carregouTudo.then((resolucao) => {
   console.log(resolucao)
 })
-
-console.log(retorno)
-
-//catch: fica esperando a promise ser rejeitada
-const retorno2 = promessa2.then(resolucao => {
-  resolucao.profissao = 'Dev Software'
-  return resolucao
-}).then(resolucao => {
-  console.log(resolucao)
-}).catch(rejeitada => [
-  console.log(rejeitada)
-])
-
-console.log(retorno)
-
-// catch dentro de then:
-promessa.then(resolucao => {
-  console.log(resolucao);
-}, reject => {
-  console.log(reject);
-});
-
-//finally: sempre executado
-promessa.then(resolucao => {
-  console.log(resolucao);
-}, reject => {
-  console.log(reject);
-}).finally(
-  console.log("Sempre executado!")
-)
