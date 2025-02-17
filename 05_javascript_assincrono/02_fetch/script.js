f28dbfa824747f426b021587e83a0a810ec4aa0a
Camila Alves <alvescamila87@gmail.com>
2025-02-16 21:40:56 -0300

    fetch json and text

--- 05_javascript_assincrono/02_fetch/script.js
+++ 05_javascript_assincrono/02_fetch/script.js
@@ -15,3 +15,25 @@ doc.then(resolucao => {
   conteudo.innerText = body
   //console.log(body)
 })
+
+const cep1 = fetch('https://viacep.com.br/ws/89030311/json');
+
+//text()
+cep1
+.then(resolve => resolve.text())
+.then(body => {
+  console.log(body)
+  const conteudo = document.querySelector('.conteudo2');
+  conteudo.innerText = body
+})
+
+//json()
+const cep2 = fetch('https://viacep.com.br/ws/89030311/json');
+cep2
+.then(resolve => resolve.json())
+.then(body => {
+  console.log(body)
+  const conteudo = document.querySelector('.conteudo3');
+  conteudo.innerText = body.logradouro
+})
+
