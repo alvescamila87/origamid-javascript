233cbe52221e4ad37e0d654826fe07adeed277de
Camila Alves <alvescamila87@gmail.com>
2025-02-23 17:36:29 -0300

    json, parse, stringgify

--- 05_javascript_assincrono/05_json/script.js
+++ 05_javascript_assincrono/05_json/script.js
@@ -1,19 +1,31 @@
-//json
+//json método de fetch
 fetch('./dados.json')
 .then(response => response.json())
 .then(json => {
-    //console.log(json[0].aula)
-    json.forEach(materia => {
-        console.log(materia.tempo)
-    })
+    console.log(json) // retorna objeto JS
+    console.log(json[0].aula)
+    json.forEach(item => console.log(item.aula))
 })
 
-//json parse
-
+//JSON.parse()
 fetch('./dados.json')
 .then(response => response.text())
 .then(jsonText => {
-    const jsonJS = JSON.parse(jsonText)
-    //console.log(jsonText)
-    console.log(jsonJS)
+    console.log(jsonText) // exibe os dados json em text
+    const jsonConterverTextParaJs = JSON.parse(jsonText)
+    console.log(jsonConterverTextParaJs) // converteu de text retorna objeto JS
 })
+
+//JSON.stringify()
+const configuracoes = {
+    empresa: "Google",
+    tempo: 25.5,
+    localizacao: "EUA"
+}
+
+const stringConfig = JSON.stringify(configuracoes)
+console.log(stringConfig) // converteu objeto para string JSON
+
+// Permite salvar um item inteiro como 'string'
+// localStorage.config = JSON.stringify(configuracoes);
+// console.log(JSON.parse(localStorage.config))
