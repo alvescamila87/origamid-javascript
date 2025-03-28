72639e6a2875b5fe77f500ce8205f72639954b08
Camila Alves <alvescamila87@gmail.com>
2025-03-28 20:49:21 -0300

    feat: await await with promise: response and json

--- 05_javascript_assincrono/07_async_await/script.js
+++ 05_javascript_assincrono/07_async_await/script.js
@@ -1,33 +1,47 @@
 //Async await
 
-async function puxarDados() {
-    try {
-        const responseDados = await fetch('./dados.json') //promise
+// async function puxarDados() {
+//     try {
+//         const responseDados = await fetch('./dados.json') //promise
     
-        const jsonDados = await responseDados.json(); //promise
+//         const jsonDados = await responseDados.json(); //promise
     
-        //console.log(responseDados)
-        //console.log(jsonDados)
-        document.body.innerText = jsonDados.aula
+//         //console.log(responseDados)
+//         //console.log(jsonDados)
+//         document.body.innerText = jsonDados.aula
 
-    } catch (erro) {
-        console.log(erro)
-    }
+//     } catch (erro) {
+//         console.log(erro)
+//     }
     
+// }
+
+// puxarDados()
+
+// // usando THEN
+
+// function iniciarFetch() {
+//     fetch('./clientes.json')
+//     .then(response => response.json())
+//     .then(dadosJson => {
+//         //console.log(dadosJson)
+//         const body = document.body.innerText = dadosJson.nome
+//         console.log(body)
+//     })
+// }
+
+// iniciarFetch()
+
+// usar await simultaneamente
+async function buscarDados() {
+    const responseClientes = fetch("./clientes.json");
+    const responseDados = fetch("./dados.json");
+
+    const jsonClientes = await (await responseClientes).json();
+    const jsonDados = await (await responseDados).json();
+
+    console.log(jsonClientes)
+    console.log(jsonDados);
 }
 
-puxarDados()
-
-// usando THEN
-
-function iniciarFetch() {
-    fetch('./clientes.json')
-    .then(response => response.json())
-    .then(dadosJson => {
-        //console.log(dadosJson)
-        const body = document.body.innerText = dadosJson.nome
-        console.log(body)
-    })
-}
-
-iniciarFetch()
+buscarDados();
